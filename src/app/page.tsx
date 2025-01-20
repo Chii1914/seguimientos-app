38c31f8e73c5020df26bd2930f6a834ee53246bb
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2025-01-20 16:26:28 -0300

    update authentication URL and remove unused state and API call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import Image from "next/image";
 import url from "./main/lib/const";
+import __url from "./main/lib/const";
 
 export default function Home() {
-  const [uwu, setUwu] = useState<any[]>([])
   
   const Router = useRouter();
   const login = () => {
-    window.location.href = "http://localhost:3000/api/auth/google";
+    window.location.href = `${__url}/auth/google`;
   }
 
   const Item = styled(Paper)(({ theme }) => ({
@@ -23,16 +23,7 @@ export default function Home() {
     color: theme.palette.text.secondary,
 
   }));
-  
-  useEffect(() => {
-    axios.get(`${url}/student/uwu`)
-      .then(response => {
-        setUwu(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching users:', error);
-      });
-  }); 
+
    return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Box>
@@ -44,9 +35,7 @@ export default function Home() {
             >
               Sistema de seguimientos académicos uv
             </Typography>
-            <h1>
-              {uwu ? uwu : ':c'}
-            </h1>
+           
             <Typography>
               A continuación, ingrese con su correo institucional UV
             </Typography>
