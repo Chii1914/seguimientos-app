e00304d22d3833ba4c06a0c9c00a03d92a32c0f5
EddieKiller <nataniel.palacios@alumnos.uv.cl>
2025-01-20 12:35:01 -0300

    fonts

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,6 +1,20 @@
 'use client';
+import localFont from 'next/font/local';
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
+
+const Swiss721BT = localFont({
+  src: [
+    { path: '../Utils/Swiss721BT.ttf', weight: '400' },
+    { path: '../Utils/Swiss721BT italic.ttf', weight: '400'},
+    { path: '../Utils/Swiss721BT bold.ttf', weight: '700' },
+    { path: '../Utils/Swiss721BT bold italic.ttf', weight: '700'},
+    { path: '../Utils/Swiss721BT CN.ttf', weight: '400' },
+    { path: '../Utils/Swiss721BT CN bold.ttf', weight: '700' },
+    { path: '../Utils/Swiss721BT CN bold italic.ttf', weight: '700'},
+  ],
+});
+
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -14,4 +28,4 @@ const theme = createTheme({
   },
 });
 
-export default theme;
+export default Swiss721BT;
